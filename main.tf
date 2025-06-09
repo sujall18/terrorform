5a9986c26af8b3cadfd3b83135262302e1497e8a
sujall18 <sujalblaze15@gmail.com>
2025-06-09 06:32:37 +0000

    ccommit

--- main.tf
+++ main.tf
@@ -28,3 +28,46 @@ module "vpc" {
   public_subnet_cidrs   = ["10.0.2.0/24"]
   name                  = "sujal-demo-vpc"
 }
+
+
+// Multi Stage Environment
+
+# dev 
+
+module "dev-app" {
+    source = "./modules/ec2"
+    my_env = "dev"
+    instance_type = "t2.micro"
+    ami = "ami-0e9085e60087ce171" 
+}
+
+#prd
+module "prd-app" {
+    source = "./modules/ec2"
+    my_env = "prd"
+    instance_type = "t2.medium"
+    ami = "ami-0e9085e60087ce171" 
+}
+
+#stg
+module "stg-app" {
+    source = "./modules/ec2"
+    my_env = "stg"
+    instance_type = "t2.small"
+    ami = "ami-0e9085e60087ce171" 
+  
+}
+output "dev_app_public_ips" {
+  value = module.dev-app.ec2_instances_public_ips
+  description = "Public IPs of the dev environment EC2 instances"
+}
+
+output "prd_app_public_ips" {
+  value = module.prd-app.ec2_instances_public_ips
+  description = "Public IPs of the prd environment EC2 instances"
+}
+
+output "stg_app_public_ips" {
+  value = module.stg-app.ec2_instances_public_ips
+  description = "Public IPs of the stg environment EC2 instances"
+}
